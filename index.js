38a45584d449c1cfc52995ef2be5e5fd91a0bda1
BREgitLEEhub2222 <>
2021-05-04 03:34:15 +0000

    sutff

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const url = prompt('website url')
 const html = fetch(url).then(function (res) {
   return res.text()
 }).then(function (text) {
-  if (text.replace(' ','').toUpperCase().includes('RICKROLL') || text.replace(' ', '').includes('youtube.com/watch?v=dQw4w9WgXcQ')) {
+  if (text.replace(' ','').toUpperCase().includes('RICKROLL') || text.replace(' ', '').includes('youtube.com/watch?v=dQw4w9WgXcQ') || text.replace(' ', '').includes('youtu.be/dQw4w9WgXcQ')) {
     console.log('this page contains rick rolling')
   } else {
-    console.log('this page does not contain rick rolling')
+    console.log('this page may not contain rick rolling')
   }
 
 });
